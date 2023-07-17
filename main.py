056dfead69911c15e69e6a3f303ab5f19e55cc2b
itamarco <itc404@gmail.com>
2023-07-17 17:56:09 +0300

    telegram webhook setting

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import os
+
+import telebot
 import uvicorn
 from fastapi import FastAPI
 
+from telegram_bot import bot, TELEGRAM_TOKEN
+
+DOMAIN = os.environ.get("DOMAIN")
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/status")
 def root():
-    return {"message": "Hello World"}
+    return {"status": "Healthy"}
 
+
+# Process webhook calls
+@app.post(f'telegram-hook/{TELEGRAM_TOKEN}/')
+def process_webhook(update: dict):
+    if update:
+        update = telebot.types.Update.de_json(update)
+        bot.process_new_updates([update])
+    else:
+        return
+
+
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(f"{DOMAIN}/telegram-hook")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
