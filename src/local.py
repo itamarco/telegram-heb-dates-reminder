5a59dc58c79f0395c089030e5b4c7597be1eadab
itamarco <itc404@gmail.com>
2023-07-17 09:45:26 +0300

    split local.py and main.py

---
+++ src/local.py
@@ -0,0 +1,4 @@
+from telegram_bot import bot
+
+print("Starting bot")
+bot.infinity_polling()
--- src/main.py
+++ src/main.py
@@ -7,14 +7,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-print("Starting bot")
-bot.infinity_polling()
-
-# import asyncio
-# import uvicorn
-#
-# asyncio.run(bot.polling())
-#
-# uvicorn.run(app)
