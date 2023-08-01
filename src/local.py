b588ec377b94a7e946d0d43d837375c395d18f81
itamarco <itc404@gmail.com>
2023-08-01 09:10:29 +0300

    local run

--- src/local.py
+++ src/local.py
@@ -1,11 +1,15 @@
 import logging
+from threading import Thread
 import uvicorn
-
 from telegram_bot import bot
 
-print("Starting bot")
 
-bot.infinity_polling()
+def blocking():
+    print("Starting bot")
+    bot.infinity_polling()
+
 
 if __name__ == "__main__":
+    thread = Thread(target=blocking)
+    thread.start()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level=logging.INFO)
