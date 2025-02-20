e8f45ad3a2a1476de00b87ce088573ec011fdd33
itamarc <itamarc@nexite.io>
2025-02-20 08:42:40 +0200

    Update PostgreSQL database URL environment variable

--- app/db.py
+++ app/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 from models.reminder import Base, ReminderDAO
 
-POSTGRES_URL = os.environ.get("SQLALCHEMY_POSTGRES_URL")
+POSTGRES_URL = os.environ.get("POSTGRES_DATABASE_URL_UNPOOLED")
 engine = create_engine(POSTGRES_URL)  # ('sqlite:///reminders.db')
 Base.metadata.create_all(engine)
 
